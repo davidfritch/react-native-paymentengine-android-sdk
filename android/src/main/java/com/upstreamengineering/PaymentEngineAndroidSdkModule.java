fc1cb107a459c234ba3d43b33fa17856fffe7de3
David Fritch <davidfritch@7oaksgroup.com>
2018-10-25 14:32:35 -0600

    Initial Draft of importing the SDK lib as a dependency, using code that references it.  But the real goal of this commit is to call the native android module code and see the log events get emitted and consume those from the ReactJS side so I can see the logs.

--- android/src/main/java/com/upstreamengineering/PaymentEngineAndroidSdkModule.java
+++ android/src/main/java/com/upstreamengineering/PaymentEngineAndroidSdkModule.java
@@ -1,10 +1,23 @@
 package com.upstreamengineering;
 
+import android.support.annotation.Nullable;
 import android.widget.Toast;
 
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.usaepay.middleware.interfaces.OnLog;
+import com.usaepay.middleware.publicclasses.UEMiddlewareInterface;
+import com.usaepay.middleware.struct.UEMTransactionResult;
+import com.usaepay.middleware.struct.UE_ERROR;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,5 +47,130 @@ public class PaymentEngineAndroidSdkModule extends ReactContextBaseJavaModule {
   @ReactMethod
   public void show(String message, int duration) {
     Toast.makeText(getReactApplicationContext(), message, duration).show();
+  }
+
+  private void emitLog(String message) {
+    WritableMap params = new WritableNativeMap();
+    params.putString("message", message);
+    getReactApplicationContext()
+            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+            .emit("onLog", params);
+  }
+
+  @ReactMethod
+  public void startTransaction(
+//      final String mConnType,
+//      final String mLog,
+//      final String sourceKey,
+//      final String pin,
+//      final String host,
+//      final ReadableMap transInfo,
+//      final Promise promise
+
+          // Just for initial test
+        final String foo,
+        final Promise promise
+  ) {
+    try {
+//      emitLog("Starting Transaction...");
+      emitLog("Starting Transaction...: " + foo);
+
+//      final HashMap<String, String> hashTransInfo = new HashMap<>();
+//      final HashMap<String, Object> hashStringObjTransInfo = transInfo.toHashMap();
+//      for (String key : hashStringObjTransInfo.keySet()) {
+//        final Object obj = hashStringObjTransInfo.get(key);
+//        hashStringObjTransInfo.put(key, obj.toString());
+//      }
+
+      emitLog("Initializing TransactionHelper...");
+
+//    final TransactionHelper transactionHelper = new TransactionHelper(
+//            getCurrentActivity(),
+//            mConnType,
+//            mLog,
+//            sourceKey,
+//            pin,
+//            host,
+//            hashTransInfo,
+//            new OnLog() {
+//              @Override
+//              public void onLog(String s) {
+//                emitLog("Log - " + s);
+//              }
+//
+//              @Override
+//              public void onLogTrace(String s) {
+//                emitLog("Trace - " + s);
+//              }
+//            }
+//    );
+//
+//    UEMiddlewareInterface ueMiddlewareInterface = new UEMiddlewareInterface() {
+//      @Override
+//      public void onConnected() {
+//
+//      }
+//
+//      @Override
+//      public void onDisconnected() {
+//      }
+//
+//      @Override
+//      public void onTransactionComplete(UEMTransactionResult uemTransactionResult) {
+//        transactionHelper.disconnect();
+//      }
+//
+//      @Override
+//      public void onDeviceInfoReceived(HashMap<String, String> hashMap) {
+//      }
+//
+//      @Override
+//      public void onError(UE_ERROR ue_error) {
+//      }
+//
+//      @Override
+//      public void onStatusChanged(String s) {
+//      }
+//
+//      @Override
+//      public void onSeePhoneNFC(HashMap<String, String> hashMap) {
+//      }
+//
+//      @Override
+//      public void onPromptForPartialAuth(String s, HashMap<String, String> hashMap) {
+//        transactionHelper.returnPartialAuthDecision();
+//      }
+//
+//      @Override
+//      public void onProgressBarUpdateAvailable(String s, String s1, float v) {
+//      }
+//
+//      @Override
+//      public void onReceiptReceived(String s) {
+//      }
+//
+//      @Override
+//      public void onMerchantCapabilitiesReceived(JSONObject jsonObject) {
+//      }
+//
+//      @Override
+//      public void onGatewayInfoReceived(JSONObject jsonObject) {
+//      }
+//    };
+//    transactionHelper.setUeMiddlewareInterface(ueMiddlewareInterface);
+//
+//    try {
+//      transactionHelper.connect();
+//      transactionHelper.startTransaction();
+//    } catch (Exception e) {
+//      Toast.makeText(getReactApplicationContext(), "An error occurred processing the transaction", Toast.LENGTH_LONG).show();
+//    }
+
+      emitLog("Transaction Completed!");
+      promise.resolve("Finished!");
+    } catch (Exception e) {
+      emitLog("Error: " + e.getLocalizedMessage());
+      promise.reject(e);
+    }
   }
 }
---
+++ android/src/main/java/com/upstreamengineering/TransactionHelper.java
@@ -0,0 +1,141 @@
+package com.upstreamengineering;
+
+import android.app.Activity;
+
+import com.usaepay.middleware.interfaces.OnLog;
+import com.usaepay.middleware.publicclasses.TerminalConfig;
+import com.usaepay.middleware.publicclasses.UEMiddleware;
+import com.usaepay.middleware.publicclasses.UEMiddlewareInterface;
+
+import java.util.HashMap;
+
+/**
+ * Copied and modified from MP200_public_demo
+ *
+ * TransactionHelper SubClass
+ *
+ * Implements callback methods called during a transaction.
+ */
+class TransactionHelper {
+
+    private UEMiddleware ueMiddleware;
+    private Activity parent;
+
+    /**
+     *  Fields to initialize UsaEpay Helper
+     */
+    private String mConnType;
+    private String mLog;
+    private String sourceKey;
+    private String pin;
+    private String host;
+    private HashMap<String, String> transInfo;
+    private UEMiddlewareInterface ueMiddlewareInterface;
+    private OnLog onLog;
+
+    /**
+     * Constructor
+     */
+    TransactionHelper(
+            Activity parent,
+            String mConnType,
+            String mLog,
+            String sourceKey,
+            String pin,
+            String host,
+            HashMap<String, String> transInfo,
+            OnLog onLog
+    ) {
+        if (parent == null) {
+            throw new IllegalArgumentException("parent is null");
+        }
+
+        if (mConnType == null) {
+            throw new IllegalArgumentException("mConnType is null");
+        }
+
+        if (mLog == null) {
+            throw new IllegalArgumentException("mLog is null");
+        }
+
+        if (sourceKey == null) {
+            throw new IllegalArgumentException("sourceKey is null");
+        }
+
+        if (pin == null) {
+            throw new IllegalArgumentException("pin is null");
+        }
+
+        if (host == null) {
+            throw new IllegalArgumentException("host is null");
+        }
+
+        if (transInfo == null) {
+            throw new IllegalArgumentException("transInfo is null");
+        }
+
+        if (onLog == null) {
+            throw new IllegalArgumentException("onLog is null");
+        }
+
+        this.parent = parent;
+        this.mConnType = mConnType;
+        this.sourceKey = sourceKey;
+        this.pin = pin;
+        this.host = host;
+        this.transInfo = transInfo;
+        this.onLog = onLog;
+
+    }
+
+    /**
+     * Drives the transaction process.
+     */
+    void connect() {
+        ueMiddleware = new UEMiddleware("MP200", parent, ueMiddlewareInterface, onLog);
+
+        // Set Terminal Config
+        TerminalConfig tc = TerminalConfig.getTipAdjustConfig();
+        //TerminalConfig tc = TerminalConfig.getAllCVMConfig();
+        //tc.setEnable_debit_msr(true);
+
+        ueMiddleware.setTerminalConfig(tc);
+
+        try {
+            // Connect to device.
+            ueMiddleware.connect(mConnType, sourceKey, pin, host);
+        } catch (Exception e) {
+            e.printStackTrace();
+            onLog.onLogTrace(e.getLocalizedMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Explicitly disconnects the device from phone/tablet.
+     */
+    void disconnect() {
+        ueMiddleware.disconnect();
+    }
+
+    void startTransaction() {
+        ueMiddleware.startTransaction(transInfo, sourceKey, pin, false, false);
+    }
+
+    void cancelTransaction() {
+        ueMiddleware.cancelTransaction();
+    }
+
+    void returnPartialAuthDecision() {
+        // Tell the middleware you want to keep the transaction (onTransactionComplete will then be called)
+        ueMiddleware.returnPartialAuthDecision(true);
+    }
+
+    void setUeMiddlewareInterface(UEMiddlewareInterface ueMiddlewareInterface) {
+        if (ueMiddlewareInterface == null) {
+            throw new IllegalArgumentException("ueMiddlewareInterface is null");
+        }
+
+        this.ueMiddlewareInterface = ueMiddlewareInterface;
+    }
+}
